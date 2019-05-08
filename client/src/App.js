83c896217894d880e64a175784b9682a82d96675
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-05-08 16:21:28 -0500

    inital commit with creat-react-app framework

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style={{fontSize: "75px", margin: "20px"}}>Google Book Search.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
